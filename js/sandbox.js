9fed5c96fa99d12793dda0ac25b5d7fd0a54ba94
Benito <bbxblack@gmail.com>
2021-03-16 10:48:29 +0100

    v0.5 sandbox

---
+++ js/sandbox.js
@@ -0,0 +1,18 @@
+
+// collaudo funzione randomNum (funziona nel modo corretto)
+//
+// function randomNum() {
+//   var min = 1;
+//   var max = (100 - min) + 1;
+//   var rNum = Math.floor(Math.random()* max) + 1;
+//   return rNum;
+// }
+
+
+
+
+
+
+var result = randomNum();
+
+console.log(result);
